06c75f2a32035f07e52fb756a8a11134a2a2d723
Shardul Janaskar <sharduljanaskar@lpc-depaulsecure-219-4.depaulsecure-student.depaul.edu>
2025-08-20 14:13:57 -0500

    created config file for api key management

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+from typing import Optional
+from pydantic import BaseModel
+
+class Config(BaseModel):
+    groq_api_key: Optional[str] = None
+    qdrant_url: Optional[str] = None
+    qdrant_api_key: Optional[str] = None
+    
+    def load_from_env(self):
+        self.groq_api_key = os.getenv("GROQ_API_KEY")
+        self.qdrant_url = os.getenv("QDRANT_URL")
+        self.qdrant_api_key = os.getenv("QDRANT_API_KEY")
+    
+    def is_configured(self) -> bool:
+        return bool(self.groq_api_key and self.qdrant_url and self.qdrant_api_key)
+
+config = Config()
+config.load_from_env()
